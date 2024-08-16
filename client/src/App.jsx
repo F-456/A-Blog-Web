4f4f2541842fc5df9ea0faaed61ab4d7885cf0f5
FSTIEW <fstiew@gmail.com>
2024-08-16 13:41:04 +0800

    Linking backend and frontend
    -Success to display info from backend
    -using axios library to get request from  backend
    -update array from backend api
    -displaying it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,27 @@
-import Header from "./Header.jsx"
+import React, { useEffect, userEffect, userState, useState } from 'react';
+import Header from "./Header.jsx";
 import Footer from "./footer.jsx";
-import Card from "./Card.jsx"
+import Card from "./Card.jsx";
+import axios from "axios"
 
 //main function that ties everyting together
 function App() {
+  //create a empty array for backend to display it 
+  const [array, setArray] = useState([]);
+
+
+  const fetchAPI = async () => {
+    const response = await axios.get("http://localhost:3001/api")
+    setArray(response.data.users);
+    console.log(response.data.users);
+
+  };
+
+  useEffect(() => {
+    fetchAPI();
+
+    //empty array ensure it only runs in initial render 
+  }, [])
   return (
     <>
       <Header></Header>
@@ -16,7 +34,16 @@ function App() {
         <Card></Card>
         <Card></Card>
         <Card></Card>
-        <Card></Card>
+      </div>
+      {/* reading array from backend and displaying on react*/}
+      <div>
+        {array.map((users, index) => (
+          <div key={index}>
+            <p>
+              {users}
+            </p>
+          </div>
+        ))}
       </div>
       <Footer></Footer>
     </>
---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const corsOption = {
+    origin: ["http://localhost:5173"],
+};
+
+app.use(cors(corsOption));
+
+app.get("/api", (req, res) => {
+    res.json({ "users": ["Userone", "Usertwo", "Userthree"] })
+})
+
+app.listen(3001, () => {
+    console.log("Server started on port 3001")
+});
