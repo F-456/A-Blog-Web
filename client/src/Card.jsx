3f96c505e00954e71790d6360e520acb60408789
FSTIEW <fstiew@gmail.com>
2024-08-18 00:00:36 +0800

    modify project for routing
    -added pages folder for routing
    -added a new home jsx file
    -modify app jsx into routing only

--- client/src/Card.jsx
+++ client/src/Card.jsx
@@ -3,10 +3,10 @@ import Atreeimage from './assets/A tree.jpg'
 
 function Card() {
     return (
-        <div className="card">
-            <img className="card-image" src={Atreeimage} alt="A tree"></img>
-            <h2 className="card-title">Blog title</h2>
-            <p className="card-text">This should be some blog phrases</p>
+        <div class="card">
+            <img class="card-image" src={Atreeimage} alt="A tree"></img>
+            <h2 class="card-title">Blog title</h2>
+            <p class="card-text">This should be some blog phrases</p>
         </div>
     );
 
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, userEffect, userState, useState } from 'react';
+import Header from "../Header.jsx";
+import Footer from "../footer.jsx";
+import Card from "../Card.jsx";
+import axios from "axios"
+
+function Home() {
+    //create a empty array for backend to display it 
+    const [array, setArray] = useState([]);
+
+
+    const fetchAPI = async () => {
+        const response = await axios.get("http://localhost:3001/api")
+        setArray(response.data.users);
+        console.log(response.data.users);
+
+    };
+
+    useEffect(() => {
+        fetchAPI();
+
+        //empty array ensure it only runs in initial render 
+    }, [])
+    return (
+        <>
+            <Header></Header>
+            <hr></hr>
+            <div className="group-card">
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+            </div>
+            {/* reading array from backend and displaying on react*/}
+            <div>
+                {array.map((users, index) => (
+                    <div key={index}>
+                        <p>
+                            {users}
+                        </p>
+                    </div>
+                ))}
+            </div>
+            <Footer></Footer>
+        </>
+
+    );
+}
+
+export default Home
